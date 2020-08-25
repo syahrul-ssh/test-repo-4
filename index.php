6c1e7cafd377a79d8bf1c4f287e6d10fbb3d1ab4
Syahrul Safarudin Hasan <syahrul.safarudinh9@gmail.com>
2020-08-25 20:29:07 +0700

    menambah list

--- index.php
+++ index.php
@@ -10,6 +10,9 @@
 <body>
     <h1>Hello World!!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, nam recusandae? Temporibus repudiandae tempore molestiae, reprehenderit libero ducimus placeat quae reiciendis omnis facilis maiores esse deleniti maxime magni a at.</p>
+    <ul>
+        <li>aku aku</li>
+    </ul>
 </body>
 
 </html>
